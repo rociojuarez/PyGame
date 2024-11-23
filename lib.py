0ed6c6b6f51d2f746a449cebc22500d894a47b6c
rocioj <juarez.rocio.a@gmail.com>
2024-11-23 13:52:46 -0300

    Se arreglan bugs

--- lib.py
+++ lib.py
@@ -1,78 +1,96 @@
 from constants import *
 import random
 import pygame
-import sys
-
-# Clase para la celda
-class Celda:
-    def __init__(self):
-        self.hay_mina = False
-        self.revelada = False
-        self.flag = False
-        self.minas_adyacentes = 0
+import os
 
 # Crear la matriz
-def crear_tablero():
-    tablero = [[Celda() for _ in range(TAMAÑO_MATRIZ)] for _ in range(TAMAÑO_MATRIZ)]
+def crear_tablero(celda:dict):
+    tablero = inicializar_matriz(celda)
     colocar_minas(tablero)
     calcular_minas_adyacentes(tablero)
     return tablero
+
+def inicializar_matriz(celda: dict)->list:
+    matriz = []
+    for i in range(CANTIDAD_FILAS):
+        fila = []
+        for j in range(CANTIDAD_COLUMNAS):
+            fila.append(celda.copy())
+        matriz.append(fila)
+    return matriz
+
 
 # Colocar minas aleatoriamente
 def colocar_minas(tablero):
     minas_colocadas = 0
     while minas_colocadas < CANTIDAD_MINAS:
-        x = random.randint(0, TAMAÑO_MATRIZ - 1)
-        y = random.randint(0, TAMAÑO_MATRIZ - 1)
-        if not tablero[y][x].hay_mina:
-            tablero[y][x].hay_mina = True
+        x = random.randint(0, CANTIDAD_COLUMNAS - 1)
+        y = random.randint(0, CANTIDAD_FILAS - 1)
+        if not tablero[y][x]["hay_mina"]:
+            tablero[y][x].update({"hay_mina": True})
             minas_colocadas += 1
 
 # Calcular minas adyacentes
 def calcular_minas_adyacentes(tablero):
-    for y in range(TAMAÑO_MATRIZ):
-        for x in range(TAMAÑO_MATRIZ):
-            if tablero[y][x].hay_mina:
-                for dy in range(-1, 2):
-                    for dx in range(-1, 2):
-                        if 0 <= x + dx < TAMAÑO_MATRIZ and 0 <= y + dy < TAMAÑO_MATRIZ:
-                            if not tablero[y + dy][x + dx].hay_mina:
-                                tablero[y + dy][x + dx].minas_adyacentes += 1
+    for y in range(CANTIDAD_FILAS):
+        for x in range(CANTIDAD_COLUMNAS):
+            if tablero[y][x]["hay_mina"]:
+                for desplazamiento_fila in range(-1, 2):
+                    for desplazamiento_columna in range(-1, 2):
+                        fila_vecina = y + desplazamiento_fila
+                        columna_vecina = x + desplazamiento_columna
+                        if 0 <= fila_vecina < CANTIDAD_FILAS and 0 <= columna_vecina < CANTIDAD_COLUMNAS:
+                            if not tablero[fila_vecina][columna_vecina]["hay_mina"]:
+                                tablero[fila_vecina][columna_vecina]["minas_adyacentes"] += 1
+
+def inicializar_matriz_ceros()->list:
+    matriz = []
+    for i in range(CANTIDAD_FILAS):
+        fila = []
+        for j in range(CANTIDAD_COLUMNAS):
+            fila.append(0)
+        matriz.append(fila)
+    return matriz
 
 # Crear matriz dinámica con -1 y 0
 def crear_matriz_dinamica(tablero):
-    matriz = [[0 for _ in range(TAMAÑO_MATRIZ)] for _ in range(TAMAÑO_MATRIZ)]
-    for y in range(TAMAÑO_MATRIZ):
-        for x in range(TAMAÑO_MATRIZ):
-            if tablero[y][x].hay_mina:
+    matriz = inicializar_matriz_ceros()
+    for y in range(CANTIDAD_FILAS):
+        for x in range(CANTIDAD_COLUMNAS):
+            if tablero[y][x]["hay_mina"]:
                 matriz[y][x] = -1
-            elif tablero[y][x].minas_adyacentes == 0:
+            elif tablero[y][x]["minas_adyacentes"] == 0:
                 matriz[y][x] = 0
-            else:
-                matriz[y][x] = tablero[y][x].minas_adyacentes
     return matriz
 
 # Modificar ceros en la matriz según las minas contiguas
 def modificar_ceros(matriz, tablero):
-    for y in range(TAMAÑO_MATRIZ):
-        for x in range(TAMAÑO_MATRIZ):
-            if matriz[y][x] == 0 and not tablero[y][x].hay_mina:
-                conteo_adyacente = tablero[y][x].minas_adyacentes
+    for y in range(CANTIDAD_FILAS):
+        for x in range(CANTIDAD_COLUMNAS):
+            if matriz[y][x] == 0 and not tablero[y][x]["hay_mina"]:
+                conteo_adyacente = tablero[y][x]["minas_adyacentes"]
                 if conteo_adyacente > 0:
                     matriz[y][x] = conteo_adyacente
 
 def calcular_vacios(tablero, fila , columna):
-    for y in range(TAMAÑO_MATRIZ):
-        for x in range(TAMAÑO_MATRIZ):
-            if 0 <= y < TAMAÑO_MATRIZ and 0 <= x < TAMAÑO_MATRIZ:
-                if es_vecino(columna, fila, x, y):
-                    if tablero[y][x].minas_adyacentes == 0 and tablero[y][x].hay_mina == False:
-                        tablero[y][x].revelada = True
-                    elif tablero[y][x].minas_adyacentes > 0 and tablero[y][x].hay_mina == False:
-                        tablero[y][x].revelada = True
+    if not (0 <= fila < TAMAÑO_MATRIZ and 0 <= columna < TAMAÑO_MATRIZ):
+        return
+    celda_actual = tablero[fila][columna]
 
-def es_vecino(columna, fila, columna2, fila2):
-    return abs(columna - columna2) <= 1 and abs(fila - fila2) <= 1
+    if celda_actual["revelada"] or celda_actual["hay_mina"] or celda_actual["flag"]:
+        return
+    celda_actual["revelada"] = True
+    if celda_actual["minas_adyacentes"] > 0:
+        return
+    for desplazamiento_fila in range(-1, 2):
+        for desplazamiento_columna in range(-1, 2):
+            # Saltar la misma celda
+            if desplazamiento_fila == 0 and desplazamiento_columna == 0:
+                continue
+            fila_vecina = fila + desplazamiento_fila
+            columna_vecina = columna + desplazamiento_columna
+            calcular_vacios(tablero, fila_vecina, columna_vecina)
+
 
 # Función para dibujar el tablero
 def dibujar_tablero(tablero, pantalla, final):
@@ -80,25 +98,25 @@ def dibujar_tablero(tablero, pantalla, final):
     imagen_marcador = pygame.transform.scale(imagen_bandera, (TAMAÑO_CELDA, TAMAÑO_CELDA))
     imagen_mina = pygame.image.load("./assets/bomba.png")
     imagen_bomba = pygame.transform.scale(imagen_mina, (TAMAÑO_CELDA, TAMAÑO_CELDA))
-    for y in range(TAMAÑO_MATRIZ):
-        for x in range(TAMAÑO_MATRIZ):
+    for y in range(CANTIDAD_FILAS):
+        for x in range(CANTIDAD_COLUMNAS):
             celda = tablero[y][x]
             rect = pygame.Rect(x * TAMAÑO_CELDA, y * TAMAÑO_CELDA + 50, TAMAÑO_CELDA, TAMAÑO_CELDA)  # Desplazar el tablero hacia abajo
             if final:
-                celda.revelada = True
-                celda.flag = False
-            if celda.revelada and not celda.flag:
+                celda["revelada"] = True
+                celda["flag"] = False
+            if celda["revelada"] and not celda["flag"]:
                 pygame.draw.rect(pantalla, BLANCO, rect)
-                if celda.hay_mina:
+                if celda["hay_mina"]:
                     pygame.draw.circle(pantalla, ROJO, rect.center, TAMAÑO_CELDA // 4)
                     pantalla.blit(imagen_bomba, rect.topleft)
-                elif celda.minas_adyacentes > 0:
+                elif celda["minas_adyacentes"] > 0:
                     fuente = pygame.font.Font(None, 36)
-                    texto = fuente.render(str(celda.minas_adyacentes), True, NEGRO)
+                    texto = fuente.render(str(celda["minas_adyacentes"]), True, NEGRO)
                     pantalla.blit(texto, rect.topleft)
-                elif celda.minas_adyacentes == 0:
-                    calcular_vacios(tablero, y, x)
-            elif celda.flag:
+                # elif celda["minas_adyacentes"] == 0:
+                #     calcular_vacios(tablero, y, x)
+            elif celda["flag"]:
                 pantalla.blit(imagen_marcador, rect.topleft)
             else:
                 pygame.draw.rect(pantalla, GRIS, rect)
@@ -115,9 +133,11 @@ def dibujar_boton_reiniciar(pantalla):
     return rect
 
 # Pantalla de inicio
-def pantalla_inicio(pantalla):
+def pantalla_inicio(pantalla)->str:
     imagen_fondo = pygame.image.load("./assets/background.jpeg").convert()
-    while True:
+    run = True
+    retorno = ""
+    while run:
         pantalla.blit(imagen_fondo, (0, 0))
         fuente = pygame.font.Font(None, 48)
 
@@ -127,84 +147,147 @@ def pantalla_inicio(pantalla):
         boton_puntajes = fuente.render("Ver Puntajes", True, NEGRO)
         boton_salir = fuente.render("Salir", True, NEGRO)
 
-        pantalla.blit(boton_jugar, (ANCHO // 2 - boton_jugar.get_width() // 2, ALTO // 2 - 60))
+        boton_juego = pantalla.blit(boton_jugar, (ANCHO // 2 - boton_jugar.get_width() // 2, ALTO // 2 - 60))
         pantalla.blit(boton_nivel, (ANCHO // 2 - boton_nivel.get_width() // 2, ALTO // 2 - 20))
         pantalla.blit(boton_puntajes, (ANCHO // 2 - boton_puntajes.get_width() // 2, ALTO // 2 + 20))
         button_quit = pantalla.blit(boton_salir, (ANCHO // 2 - boton_salir.get_width() // 2, ALTO // 2 + 60))
 
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
+                run = False
+                retorno = "salir"
             if evento.type == pygame.MOUSEBUTTONDOWN:
                 if button_quit.collidepoint(evento.pos):
-                    pygame.quit()
-                    sys.exit()
-                else:
-                    mouse_x, mouse_y = evento.pos
+                    run = False
+                    retorno = "salir"
+                elif boton_juego.collidepoint(evento.pos):
+                    run = False
+                    retorno = "jugar"
 
-                    # Verifica si se hace clic en el botón "Jugar"
-                    if (ANCHO // 2 - boton_jugar.get_width() // 2 < mouse_x < ANCHO // 2 + boton_jugar.get_width() // 2) and \
-                            (ALTO // 2 - 60 < mouse_y < ALTO // 2 - 60 + boton_jugar.get_height()):
-                        return  # Inicia el juego
 
             pygame.display.flip()
+    return retorno
 
-def puntaje(pantalla, contador_puntaje):
-    fuente = pygame.font.Font(None, 25)
-    texto = fuente.render(f"Puntaje: {contador_puntaje}", True, NEGRO)
-    rect = texto.get_rect(center=(ANCHO - 60, 25))
-    pygame.draw.rect(pantalla, GRIS, rect.inflate(20, 10))  # Fondo del botón
-    pantalla.blit(texto, rect)
+def guardar_puntaje(ARCHIVO_PUNTAJES,nombre, puntaje):
+    """
+    Guarda el nombre del usuario y su puntaje en el archivo.
+    """
+    with open(ARCHIVO_PUNTAJES, "a") as archivo:
+        archivo.write(f"{nombre},{puntaje}\n")
+
+def obtener_mejores_puntajes(ARCHIVO_PUNTAJES):
+    """
+    Obtiene los tres mejores puntajes ordenados de mayor a menor.
+    """
+    if not os.path.exists(ARCHIVO_PUNTAJES):
+        return []
+
+    with open(ARCHIVO_PUNTAJES, "r") as archivo:
+        puntajes = []
+        for linea in archivo:
+            datos = linea.strip().split(",")
+            if len(datos) == 2:
+                nombre, puntaje = datos
+                puntajes.append((nombre, int(puntaje)))
+
+    # Ordena los puntajes de mayor a menor
+    puntajes_ordenados = sorted(puntajes, key=lambda x: x[1], reverse=True)
+
+    # Retornar los tres mejores
+    return puntajes_ordenados[:3]
+
+def mostrar_puntajes():
+    """
+    Muestra los tres mejores puntajes.
+    """
+    mejores_puntajes = obtener_mejores_puntajes()
+    if mejores_puntajes:
+        print("\n** Mejores Puntajes **")
+        for i, (nombre, puntaje) in enumerate(mejores_puntajes, start=1):
+            print(f"{i}. {nombre}: {puntaje} puntos")
+    else:
+        print("\nNo hay puntajes registrados aún.")
+
+    input("\nPresiona Enter para volver al menú principal.")
 
 
-
-# Función principal del juego
-def main(pantalla):
-    pantalla_inicio(pantalla)  # Llama a la pantalla de inicio
-    tablero = crear_tablero()
+def iniciar_juego(celda, pantalla):
+    tablero = crear_tablero(celda)
     matriz_dinamica = crear_matriz_dinamica(tablero)
     modificar_ceros(matriz_dinamica, tablero)
     final = False
     contador_puntaje = 0000
-
-    while True:
+    corriendo = True
+    boton_rect = dibujar_boton_reiniciar(pantalla)
+    actualizar_pantalla = True
+    while corriendo:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
+                corriendo = False
                 pygame.quit()
-                sys.exit()
             if evento.type == pygame.MOUSEBUTTONDOWN:
                 x, y = evento.pos
-                if evento.button == 1:
-                    # Verificar si se hace clic en el botón "Reiniciar"
-                    boton_rect = dibujar_boton_reiniciar(pantalla)
-                    if boton_rect.collidepoint(evento.pos):
-                        final = False
-                        tablero = crear_tablero()  # Reiniciar el tablero
-                        continue  # Volver a la parte superior del bucle
+                if evento.button == 1 and boton_rect.collidepoint(evento.pos):
+                    tablero = crear_tablero(celda)
+                    matriz_dinamica = crear_matriz_dinamica(tablero)
+                    modificar_ceros(matriz_dinamica, tablero)
+                    final = False
+                    contador_puntaje = 0000
+                    actualizar_pantalla = True
 
-                    # Verificar si se hace clic en una celda
-                    columna, fila = x // TAMAÑO_CELDA, (y - 50) // TAMAÑO_CELDA  # Ajustar la fila
+                else:
+                    columna, fila = x // TAMAÑO_CELDA, (y - 50) // TAMAÑO_CELDA
                     if 0 <= fila < TAMAÑO_MATRIZ and 0 <= columna < TAMAÑO_MATRIZ:
-                        if not tablero[fila][columna].revelada and not tablero[fila][columna].flag:
-                            tablero[fila][columna].revelada = True
-                            if tablero[fila][columna].hay_mina:
-                                final = True
-                            else:
-                                contador_puntaje += 1
+                        if evento.button == 1:
+                            if not tablero[fila][columna]["revelada"] and not tablero[fila][columna]["flag"]:
+                                if tablero[fila][columna]["hay_mina"]:
+                                    final = True
+                                else:
+                                    contador_puntaje += 1
+                                    if tablero[fila][columna]["minas_adyacentes"] == 0:
+                                        calcular_vacios(tablero, fila, columna)
+                                    else:
+                                        tablero[fila][columna]["revelada"] = True
+                                actualizar_pantalla = True
 
-                elif evento.button == 3:
-                    columna, fila = x // TAMAÑO_CELDA, (y - 50) // TAMAÑO_CELDA  # Ajustar la fila
-                    if 0 <= fila < TAMAÑO_MATRIZ and 0 <= columna < TAMAÑO_MATRIZ:
-                        if not tablero[fila][columna].revelada and not tablero[fila][columna].flag:
-                            tablero[fila][columna].flag = True
-                        elif tablero[fila][columna].flag and not tablero[fila][columna].revelada:
-                            tablero[fila][columna].flag = False
+                        elif evento.button == 3:
+                            if not tablero[fila][columna]["revelada"]:
+                                tablero[fila][columna]["flag"] = not tablero[fila][columna]["flag"]
+                                actualizar_pantalla = True
 
-
+        if actualizar_pantalla:
             pantalla.fill(NEGRO)
             dibujar_boton_reiniciar(pantalla)
             puntaje(pantalla, contador_puntaje)
             dibujar_tablero(tablero, pantalla, final)
             pygame.display.flip()
+            actualizar_pantalla = False
+
+def puntaje(pantalla, contador_puntaje):
+    fuente = pygame.font.Font(None, 25)
+    texto = fuente.render(f"Puntaje: {str(contador_puntaje).zfill(4)}", True, NEGRO)
+    rect = texto.get_rect(center=(ANCHO - 60, 25))
+    pygame.draw.rect(pantalla, GRIS, rect.inflate(20, 10))  # Fondo del botón
+    pantalla.blit(texto, rect)
+    return None
+
+# Función principal del juego
+def main(pantalla):
+    celda = {
+        "hay_mina": False,
+        "revelada": False,
+        "flag": False,
+        "minas_adyacentes": 0
+    }
+    seleccion_inicio = pantalla_inicio(pantalla)
+    match seleccion_inicio:
+        case "salir":
+            pygame.quit()
+        case "jugar":
+            iniciar_juego(celda,pantalla)
+        case _:
+            print("error")
+
+    # ARCHIVO_PUNTAJES = "puntajes.txt"
+    #
+
