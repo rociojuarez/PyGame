78fa88547ac03995d60ce7b6d9dc4879b7118dcf
rocioj <juarez.rocio.a@gmail.com>
2024-11-26 19:26:31 -0300

    Se arreglan estilos

--- constants.py
+++ constants.py
@@ -1,13 +1,19 @@
 # Configuración de constantes
-ANCHO, ALTO = 800, 800
+ANCHO, ALTO = 700, 700
 TAMAÑO_MATRIZ = 8
-TAMAÑO_CELDA = 45
+TAMAÑO_CELDA = 40
 PATH_ARCHIVO_PUNTAJES = "./assets/puntajes.json"
 
 # Colores
 GRIS = (200, 200, 200)
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
-ROJO = (255, 0, 0)
+ROJO = (180, 0, 50)
+VERDE = (0, 180, 50)
+AZUL = (0,0,180)
+AMARILLO = (233,236,64)
+VIOLETA = (160,35,219)
+ROSA = (236, 65, 187)
+CELESTE = (20,199,199)
 FONDO = (10,10,25)
 FONDO_CELDA= (39,71,92)
